217858faf3b0c1900cebd41a562cca5c3d47563a
Daniel Santamaria <daniexv91@gmail.com>
2025-12-08 16:32:16 -0500

    Roles 2
    Inyecciones e importaciones en LayoutAdmin (asegurarse de poner Usuario)
    Inyeccion en el Listado
    condicion en el listado para especificar lo que puede hacer cada rol en el crud

--- MinimarketSantamaria/obj/Debug/net9.0/MinimarketSantamaria.AssemblyInfo.cs
+++ MinimarketSantamaria/obj/Debug/net9.0/MinimarketSantamaria.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimarketSantamaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb549382c571a24b0bc5f8bc1bd9e6341adcf098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395beafc59ef8b62df73c086cf769327621bcf62")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimarketSantamaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimarketSantamaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
