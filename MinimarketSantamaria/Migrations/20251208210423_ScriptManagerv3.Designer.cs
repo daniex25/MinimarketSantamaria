395beafc59ef8b62df73c086cf769327621bcf62
Daniel Santamaria <daniexv91@gmail.com>
2025-12-08 16:05:18 -0500

    Migracion Roles
    add-migration ScriptManagerv3
    update-database

--- MinimarketSantamaria/Migrations/20251208210423_ScriptManagerv3.Designer.cs
+++ MinimarketSantamaria/Migrations/20251208210423_ScriptManagerv3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MinimarketSantamaria.Data;
 
@@ -11,9 +12,11 @@ using MinimarketSantamaria.Data;
 namespace MinimarketSantamaria.Migrations
 {
     [DbContext(typeof(MinimarketSantamariaContext))]
-    partial class MinimarketSantamariaContextModelSnapshot : ModelSnapshot
+    [Migration("20251208210423_ScriptManagerv3")]
+    partial class ScriptManagerv3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MinimarketSantamaria/Migrations/20251208210423_ScriptManagerv3.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimarketSantamaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class ScriptManagerv3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                defaultValue: "0",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apellidos",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "AspNetUsers",
+                type: "nvarchar(120)",
+                maxLength: 120,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Dni",
+                table: "AspNetUsers",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Edad",
+                table: "AspNetUsers",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "AspNetUsers",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombres",
+                table: "AspNetUsers",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellidos",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Dni",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Edad",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nombres",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true,
+                oldDefaultValue: "0");
+        }
+    }
+}
